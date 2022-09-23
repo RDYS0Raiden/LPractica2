8430715c848cf220285a1a06ba9bcd7df491c473
RDYS0yamato <yamatotshiro@gmail.com>
2022-09-23 13:00:15 -0400

    .

--- app/src/main/java/com/example/lpractica2/Pantalla2.kt
+++ app/src/main/java/com/example/lpractica2/Pantalla2.kt
@@ -11,7 +11,7 @@ class Pantalla2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding= ActivityPantalla2Binding.inflate(layoutInflater)
         setContentView(binding.root)
-        //a
+        //hacer bigind
     }
 
 }
